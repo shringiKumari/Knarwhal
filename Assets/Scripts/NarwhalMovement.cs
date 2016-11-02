57a9d1cfe62d91e2a996251796d2e209fa20f420
Shringi <sk1382@york.ac.uk>
2016-11-02 21:57:31 +0000

    Moves in the direction Narwhal is facing, cleaned input handling

--- Assets/Scripts/NarwhalMovement.cs
+++ Assets/Scripts/NarwhalMovement.cs
@@ -2,45 +2,34 @@
 using System.Collections;
 
 public class NarwhalMovement : MonoBehaviour {
-	public float speed = 10;
-	public Transform target;
+	public float rotationSpeed = 10;
+	public float translationSpeed = 10;
+	public string move;
+	public string rotate;
+	public string dash;
+	public string spout;
+	public float hornAngle;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetButton("Up")) {
-			transform.position += new Vector3 (0, +1, 0) * Time.deltaTime;
+	void FixedUpdate () {
+		transform.Rotate (Vector3.forward * Input.GetAxis(rotate) * rotationSpeed);
+
+		if (Input.GetButton(move)) {
+
+			Vector3 ReferenceVector = Quaternion.Euler(0, 0, hornAngle) * transform.right;
+			transform.position += ReferenceVector * Time.fixedDeltaTime * translationSpeed;
 		}
-		if (Input.GetButton("Down")) {
-			transform.position += new Vector3 (0, -1, 0) * Time.deltaTime;
+
+		if (Input.GetButton(dash)) {
+			Debug.Log ("Dash" + dash);
 		}
-		if (Input.GetButton("Left")) {
-			transform.position += new Vector3 (-1, 0, 0) * Time.deltaTime;
+
+		if (Input.GetButton(spout)) {
+			Debug.Log ("Spout" + spout);
 		}
-		if (Input.GetButton("Right")) {
-			transform.position += new Vector3 (+1, 0, 0) * Time.deltaTime;
-		}
-		if (Input.GetButton("A")) {
-			Debug.Log ("A");
-		}
-		if (Input.GetButton("B")) {
-			Debug.Log ("B");
-		}
-		if (Input.GetButton("X")) {
-			Debug.Log ("X");
-		}
-		if (Input.GetButton("Y")) {
-			Debug.Log ("Y");
-		}
-		if (Input.GetButton("RB")) {
-			Debug.Log ("RB");
-		}
-		//Debug.Log(Input.GetAxis("RightJoystickX"));
-		transform.Rotate (Vector3.forward * Input.GetAxis("RightJoystickX") * speed);
-		float angle = Quaternion.Angle(transform.rotation, target.rotation);
-		Debug.Log (angle);
 	}
 }
