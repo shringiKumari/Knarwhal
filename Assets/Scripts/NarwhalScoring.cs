641feb61250739de768f91fbfda310fbbac8ce48
Thryn Henderson <thend012@cgs-macd-15186.campus.goldsmiths.ac.uk>
2016-11-03 12:28:06 +0000

    added whale collision and horn scoring debug

---
+++ Assets/Scripts/NarwhalScoring.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NarwhalScoring : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("hit point");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
