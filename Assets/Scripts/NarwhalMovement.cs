21ad8c507c0b31e4c1e415378f6e5c3abc9b471b
Thryn Henderson <cvh506@york.ac.uk>
2016-11-04 10:46:23 +0000

    fixed versioning conflicts and added scene limits

--- Assets/Scripts/NarwhalMovement.cs
+++ Assets/Scripts/NarwhalMovement.cs
@@ -10,23 +10,44 @@ public class NarwhalMovement : MonoBehaviour {
 	public string spout;
 	public float hornAngle;
 	public float thrust;
-
+	private float botLimit, topLimit, leftLimit, rightLimit;
 	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0,0, camDistance));
+		Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1,1, camDistance));
+
+		botLimit = bottomCorner.x;
+		topLimit = topCorner.x;
+		leftLimit = bottomCorner.y;
+		rightLimit = topCorner.y;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		transform.Rotate (Vector3.forward * Input.GetAxis(rotate) * rotationSpeed);
 
+		Vector3 pos = transform.position;
+		if (pos.x < botLimit)
+			pos.x = botLimit;
+		if (pos.x > topLimit)
+			pos.x = topLimit;
+		if (pos.y < leftLimit)
+			pos.y = leftLimit;
+		if (pos.y > rightLimit)
+			pos.y = rightLimit;
+
+		transform.position = pos;
+
 		if (Input.GetButton(move)) {
 
 			Vector3 ReferenceVector = Quaternion.Euler(0, 0, hornAngle) * transform.right;
 			transform.position += ReferenceVector * Time.fixedDeltaTime * translationSpeed;
 			//Debug.Log(move);
+
 		}
 
 		if (Input.GetButton(dash)) {
