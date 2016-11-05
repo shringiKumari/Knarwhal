10c183d8c08d08a5180b7d1f2c1db2cd230661d9
nepp <0andrewmartin0@gmail.com>
2016-11-05 18:17:19 +0000

    keyboard input in code

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+class KeyboardInput {
+  KeyCode[] rotateLeft = { KeyCode.A, KeyCode.J };
+  KeyCode[] rotateRight = { KeyCode.D, KeyCode.L };
+  KeyCode[] move = { KeyCode.W, KeyCode.I };
+  KeyCode[] dash = { KeyCode.Q, KeyCode.U };
+  KeyCode[] spout = { KeyCode.E, KeyCode.O };
+
+  public float Rotate(int id) {
+    var left = Input.GetKey(rotateLeft[id]) ? 1 : 0;
+    var right = Input.GetKey(rotateRight[id]) ? -1 : 0;
+    return left + right;
+  }
+
+  public bool Move(int id) {
+    return Input.GetKey(move[id]);
+  }
+
+  public bool Dash(int id) {
+    return Input.GetKey(dash[id]);
+  }
+
+  public bool Spout(int id) {
+    return Input.GetKey(spout[id]);
+  }
+}
--- Assets/Scripts/NarwhalMovement.cs
+++ Assets/Scripts/NarwhalMovement.cs
@@ -13,9 +13,14 @@ public class NarwhalMovement : MonoBehaviour {
 	private float botLimit, topLimit, leftLimit, rightLimit;
 	public Rigidbody2D rb;
 
+  private int playerID;
+  private KeyboardInput keyboard = new KeyboardInput();
+
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D>();
+    playerID = name == "Andy" ? 0 : 1;
+
+    rb = GetComponent<Rigidbody2D>();
 		float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
 		Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0,0, camDistance));
 		Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1,1, camDistance));
@@ -25,10 +30,30 @@ public class NarwhalMovement : MonoBehaviour {
 		leftLimit = bottomCorner.y;
 		rightLimit = topCorner.y;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		transform.Rotate (Vector3.forward * Input.GetAxis(rotate) * rotationSpeed);
+
+  private float RotateInput() {
+    var r = Input.GetAxis(rotate);
+    if (r == 0f) {
+      r = keyboard.Rotate(playerID);
+    }
+    return r;
+  }
+
+  private bool MoveInput() {
+    return Input.GetButton(move) || keyboard.Move(playerID);
+  }
+
+  private bool DashInput() {
+    return Input.GetButton(dash) || keyboard.Dash(playerID);
+  }
+
+  private bool SpoutInput() {
+    return Input.GetButton(dash) || keyboard.Spout(playerID);
+  }
+
+  // Update is called once per frame
+  void FixedUpdate () {
+		transform.Rotate (Vector3.forward * RotateInput() * rotationSpeed);
 
 		Vector3 pos = transform.position;
 		if (pos.x < botLimit)
@@ -42,7 +67,7 @@ public class NarwhalMovement : MonoBehaviour {
 
 		transform.position = pos;
 
-		if (Input.GetButton(move)) {
+    if (MoveInput()) {
 
 			Vector3 ReferenceVector = Quaternion.Euler(0, 0, hornAngle) * transform.right;
 			transform.position += ReferenceVector * Time.fixedDeltaTime * translationSpeed;
@@ -50,12 +75,12 @@ public class NarwhalMovement : MonoBehaviour {
 
 		}
 
-		if (Input.GetButton(dash)) {
+		if (DashInput()) {
 			Debug.Log ("Dash" + dash);
 			rb.AddForce (transform.right * thrust);
 		}
 
-		if (Input.GetButton(spout)) {
+		if (SpoutInput()) {
 			Debug.Log ("Spout" + spout);
 		}
 	}
