ec7ef669d3fb3c18fb116378e328eb55b8ed696a
Andrew Martin <amart053@cgs-macd-15688.campus.goldsmiths.ac.uk>
2016-11-07 15:17:57 +0000

    playtest commit

--- Assets/Scripts/Skewer.cs
+++ Assets/Scripts/Skewer.cs
@@ -10,7 +10,7 @@ public class Skewer : MonoBehaviour {
   private GameObject enemyParent;
 
   private enum State {
-    hunting, pricked, skewered, cooldown
+    hunting, jabbed, skewered, cooldown
   }
 
   void SetState(State s){
@@ -20,30 +20,42 @@ public class Skewer : MonoBehaviour {
 
   void OnTriggerEnter2D(Collider2D enemy) {
     
-    if (state == State.pricked && enemy.name == "VitalConfirm") {
-      Debug.Log ("Collision with " + enemy.name);
+    if (state == State.jabbed && enemy.name == "VitalConfirm") {
       var parent = transform.parent.gameObject;
       var newEnemyParent = enemy.gameObject.transform.parent.parent.gameObject;
       if (newEnemyParent == enemyParent) {
-
+        Debug.Log (parent.name + " skewered " + enemyParent.name);
+        SetState (State.skewered);
       }
     }
 
     if (state == State.hunting && enemy.name == "VitalCollision") {
-      Debug.Log ("Collision with " + enemy.name);
       var parent = transform.parent.gameObject;
       enemyParent = enemy.gameObject.transform.parent.gameObject;
       if (parent != enemyParent) {
+        Debug.Log (parent.name + " jabbed " + enemyParent.name);
+        // Activate and configure wound
         var wound = enemyParent.transform.Find ("wound");
         wound.gameObject.SetActive (true);
-        Destroy (wound.gameObject.GetComponent<FixedJoint2D> ());
+        //Destroy (wound.gameObject.GetComponent<FixedJoint2D> ());
         wound.position = parent.transform.TransformPoint (new Vector3 (1.6f, -0.4f));
         wound.rotation = parent.transform.rotation;
-        var j = wound.gameObject.AddComponent<FixedJoint2D> ();
-        j.connectedBody = enemyParent.GetComponent<Rigidbody2D> ();
-        SetState (State.pricked);
+        //var j = wound.gameObject.AddComponent<FixedJoint2D> ();
+        //j.connectedBody = enemyParent.GetComponent<Rigidbody2D> ();
+        // Activate skewer horn sprite
+        var skewer = parent.transform.Find ("skewer");
+        skewer.gameObject.SetActive (true);
+        SetState (State.jabbed);
       }
     }
+  }
+
+  void DisableWoundAndSkewer(){
+    var wound = enemyParent.transform.Find ("wound");
+    wound.gameObject.SetActive (false);
+    var parent = transform.parent.gameObject;
+    var skewer = parent.transform.Find ("skewer");
+    skewer.gameObject.SetActive (false);
   }
 
   // Update is called once per frame
@@ -51,13 +63,15 @@ public class Skewer : MonoBehaviour {
     if (state == State.hunting) {
       // Stay in this state indefinitely
     }
-    else if (state == State.pricked) {
+    else if (state == State.jabbed) {
       if (lastChangeTime + 1 < Time.time) {
+        DisableWoundAndSkewer ();
         SetState (State.hunting);
       }
     }
     else if (state == State.skewered) {
-      if (lastChangeTime + 3 < Time.time) {
+      if (lastChangeTime + 6 < Time.time) {
+        DisableWoundAndSkewer ();
         SetState (State.cooldown);
       }
     }
