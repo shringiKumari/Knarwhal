a096a33df1e909682e8e05767f0bffd1c7485a16
Shringi <sk1382@york.ac.uk>
2016-11-08 11:24:12 +0000

    pufferfish spawn and move in random direction

--- Assets/Scripts/PufferMovement.cs
+++ Assets/Scripts/PufferMovement.cs
@@ -7,9 +7,18 @@ public class PufferMovement : MonoBehaviour {
 	private float theta = 0f;
 	private float referencePositionX = 0.01f;
 	private float referencePositionY = 0.01f;
+	private int direction = 1;
 
 	public float thetaStep = 0.5f;
 	public float translationSpeed = 30.0f;
+
+	public float pufferSpwanXmin = 0.05f;
+	public float pufferSpwanXmax = 0.9f;
+
+	public float pufferSpwanYmin = 0.05f;
+	public float pufferSpwanYmax = 0.9f;
+
+	private float spawnMidX;
 
 
 	// Use this for initialization
@@ -18,16 +27,22 @@ public class PufferMovement : MonoBehaviour {
 	}
 
 	void OnEnable () {		
-		Vector2 randomSpawnPosition = new Vector3 (Random.Range (0.05f, 0.9f), Random.Range (0.05f, 0.9f),0);
-		transform.position = Camera.main.ViewportToWorldPoint(randomSpawnPosition) - new Vector3 (0, 0, Camera.main.transform.position.z);
-		Debug.Log(transform.position);
+		Vector2 randomSpawnPosition = new Vector3 (Random.Range (pufferSpwanXmin, pufferSpwanXmax), 
+										Random.Range (pufferSpwanYmin, pufferSpwanYmax),0);
+		transform.position = Camera.main.ViewportToWorldPoint(randomSpawnPosition) - 
+										new Vector3 (0, 0, Camera.main.transform.position.z);
+		spawnMidX = (pufferSpwanXmin + pufferSpwanXmax) / 2;
+		if (transform.position.x < spawnMidX) {
+			direction = 1;
+		} else {
+			direction = -1;
+		}
 	}
-		
-	
+			
 	// Update is called once per frame
 	void FixedUpdate () {
 		theta += thetaStep;
-		Vector3 referenceVector = new Vector2 (referencePositionX, Mathf.Sin(theta) * referencePositionY);
+		Vector3 referenceVector = new Vector2 (referencePositionX * direction, Mathf.Sin(theta) * referencePositionY);
 		rb.velocity = referenceVector.normalized * Time.fixedDeltaTime * translationSpeed;
 		//Debug.Log ("Here");
 	
