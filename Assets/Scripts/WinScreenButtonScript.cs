9bc73fed4c57a605f81a114ac6509be7b49a234a
Shringi <sk1382@york.ac.uk>
2016-11-09 19:40:22 +0000

    Win screen controls added for keyboard

---
+++ Assets/Scripts/WinScreenButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScreenButtonScript : MonoBehaviour {
+
+	private GameWinScreen gameWinScreen;
+	// Use this for initialization
+	void Start () {
+
+		GameObject gameWin = GameObject.FindGameObjectWithTag("WinScreen");
+		gameWinScreen = gameWin.GetComponent<GameWinScreen>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.H)) {
+			gameWinScreen.PlayClick ();
+		}
+		if (Input.GetKeyDown(KeyCode.B)) {
+			gameWinScreen.QuitClick ();
+		}
+	
+	}
+}
