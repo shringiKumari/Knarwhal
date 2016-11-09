7c61fd538c1fc45eb4d7e46508bda22064ac204f
Thryn Henderson <thend012@cgs-macd-15686.campus.goldsmiths.ac.uk>
2016-11-09 15:33:26 +0000

    Storing changes

---
+++ Assets/Scripts/NarwhalReset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NarwhalReset : MonoBehaviour {
+
+	private GameObject[] wounds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void ClearWounds(){
+		//TODO find all stabhole(Clone) children
+		wounds = GameObject.FindGameObjectsWithTag("Wound");
+		//TODO remove wound sprites from both scene
+		foreach (GameObject wound in wounds) {
+			Destroy (wound);
+		}
+		//TODO end
+		Debug.Log("reset wounds callled");
+	}
+}
