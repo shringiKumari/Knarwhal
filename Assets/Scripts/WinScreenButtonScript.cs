2013eb1b87afb7143737e1fc29fed0a9e5bbe74d
Shringi <sk1382@york.ac.uk>
2016-11-09 19:44:19 +0000

    Win screen controls added for xbox controller

--- Assets/Scripts/WinScreenButtonScript.cs
+++ Assets/Scripts/WinScreenButtonScript.cs
@@ -15,7 +15,9 @@ public class WinScreenButtonScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.H)) {
+		if ((Input.GetKeyDown(KeyCode.H))||
+			(Input.GetButtonDown("Start"))||
+			(Input.GetButtonDown("Start_W"))){
 			gameWinScreen.PlayClick ();
 		}
 		if (Input.GetKeyDown(KeyCode.B)) {
