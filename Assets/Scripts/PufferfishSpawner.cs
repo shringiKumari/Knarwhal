d3333632bd3854b63bad3ab364446a94730a8682
Shringi <sk1382@york.ac.uk>
2016-11-10 00:20:30 +0000

    made puffefish list

--- Assets/Scripts/PufferfishSpawner.cs
+++ Assets/Scripts/PufferfishSpawner.cs
@@ -1,35 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PufferfishSpawner : MonoBehaviour {
 
 	[SerializeField]
-	private GameObject pufferfish;
+	private GameObject pufferfishReference;
 	private float startTimer = 0f;
 	private float spawnThreshholdTimer = 2f;
 
 	private float screenLimitMax = 15f;
 	private float screenLimitMin = -15f;
 
+     private List<GameObject> pufferfishList = new List<GameObject> ();
+
 	// Use this for initialization
 	void Start () {
-          screenLimitMin = pufferfish.GetComponent<PufferMovement>().pufferSpwanXLeft;
-          screenLimitMax = pufferfish.GetComponent<PufferMovement>().pufferSpwanXRight;
+          screenLimitMin = pufferfishReference.GetComponent<PufferMovement>().pufferSpwanXLeft;
+          screenLimitMax = pufferfishReference.GetComponent<PufferMovement>().pufferSpwanXRight;
+
+          pufferfishList.Add (pufferfishReference);
 		}
 	
 	// Update is called once per frame
 	void Update () {
-		if (((pufferfish.transform.position.x <= screenLimitMin) || (pufferfish.transform.position.x >= screenLimitMax))
-			&& (pufferfish.activeSelf == true)) {
-			pufferfish.SetActive (false);
-			pufferfish.transform.localScale = new Vector3 (1, 1, 0);
-			startTimer = 0f;
-		}
+          foreach (GameObject pufferfish in pufferfishList) {
+               if (((pufferfish.transform.position.x <= screenLimitMin) || (pufferfish.transform.position.x >= screenLimitMax))
+               && (pufferfish.activeSelf == true)) {
+                    pufferfish.SetActive (false);
+                    pufferfish.transform.localScale = new Vector3 (1, 1, 0);
+                    startTimer = 0f;
+               }
 
-		if ((startTimer >= spawnThreshholdTimer) && (pufferfish.activeSelf == false)){
-			pufferfish.SetActive (true);
-		}
-		startTimer += Time.deltaTime;
+               if ((startTimer >= spawnThreshholdTimer) && (pufferfish.activeSelf == false)) {
+                    pufferfish.SetActive (true);
+               }
+          }
+          startTimer += Time.deltaTime;
 
 	}
 }
